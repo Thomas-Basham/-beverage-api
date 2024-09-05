bef55970d68478a2ee3e1efa639f85d8067718e5
Thomas Basham <Bashamtg@gmail.com>
2024-09-04 17:30:12 -0700

    updated cors config

--- api/index.js
+++ api/index.js
@@ -19,92 +19,15 @@ const app = express();
 // define a port
 const PORT = 4000;
 
-const BEVERAGES = [
-  {
-    id: 1,
-    name: "Orange Juice",
-    description: "Freshly squeezed orange juice.",
-    price: 3.99,
-    category: "Juices",
-    inStock: true,
-  },
-  {
-    id: 2,
-    name: "Espresso",
-    description: "Strong and rich espresso shot.",
-    price: 2.5,
-    category: "Coffee",
-    inStock: true,
-  },
-  {
-    id: 3,
-    name: "Green Tea",
-    description: "Soothing and refreshing green tea.",
-    price: 1.99,
-    category: "Tea",
-    inStock: false,
-  },
-  {
-    id: 4,
-    name: "Lemonade",
-    description: "Chilled lemonade with a hint of mint.",
-    price: 2.75,
-    category: "Juices",
-    inStock: true,
-  },
-  {
-    id: 5,
-    name: "Cappuccino",
-    description: "Creamy cappuccino with frothy milk.",
-    price: 3.5,
-    category: "Coffee",
-    inStock: true,
-  },
-  {
-    id: 6,
-    name: "Herbal Tea",
-    description: "A calming blend of herbal teas.",
-    price: 2.25,
-    category: "Tea",
-    inStock: true,
-  },
-  {
-    id: 7,
-    name: "Smoothie",
-    description: "A thick and creamy fruit smoothie.",
-    price: 4.99,
-    category: "Juices",
-    inStock: true,
-  },
-  {
-    id: 8,
-    name: "Iced Coffee",
-    description: "Cold brewed iced coffee with a hint of vanilla.",
-    price: 2.99,
-    category: "Coffee",
-    inStock: false,
-  },
-  {
-    id: 9,
-    name: "Black Tea",
-    description: "Bold and robust black tea.",
-    price: 1.75,
-    category: "Tea",
-    inStock: true,
-  },
-  {
-    id: 10,
-    name: "Apple Juice",
-    description: "Refreshing apple juice made from fresh apples.",
-    price: 3.5,
-    category: "Juices",
-    inStock: true,
-  },
-];
 
 // Define our Middleware
 // Use CORS Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:4000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 // Use JSON middleware to parse request bodies
 app.use(express.json());
@@ -116,7 +39,7 @@ app.get("/", (request, response, next) => {
 });
 
 // Route to Get all Beverages
-app.get("/beverages", async (request, response, next) => {
+app.get("/beverages", cors(corsOptions), async (request, response, next) => {
   try {
     // response.json(BEVERAGES);
     const res = await supabase.get("/beverages");
