cba4ca0ab8f90dcea8202eef28464bec006e7c86
Thomas Basham <Bashamtg@gmail.com>
2024-09-09 17:14:33 -0700

    modularized our get all route to reduce code clutter

--- api/routes/getAll.js
+++ api/routes/getAll.js
@@ -3,8 +3,6 @@ const supabase = require("../../supabaseInstance");
 
 const getAll = async (request, response, next) => {
   try {
-    console.log(request);
-    // response.json(BEVERAGES);
     const res = await supabase.get("/beverages");
     response.json(res.data);
   } catch (error) {
